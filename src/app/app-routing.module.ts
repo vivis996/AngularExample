61f5cf9d7f5cb94ad84f1891c62bca2f2db1b3b9
Daniel Viveros <dviveros90@gmail.com>
2018-06-27 17:45:42 -0500

    Insert home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes), CommonModule ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
